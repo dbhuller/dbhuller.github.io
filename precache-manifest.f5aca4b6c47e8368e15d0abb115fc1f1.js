e77ecaea9987f4384fee346d6b6faed6544daaa9
Deep <dbhuller@mail.sfsu.edu>
2020-02-08 18:14:27 -0800

    Updates

---
+++ precache-manifest.f5aca4b6c47e8368e15d0abb115fc1f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bebfb06f94326664c791d1e07f0bfd5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bc8c0215e748e67baea",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "724f70080248f184b896",
+    "url": "/static/js/2.f5bb07c4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f5bb07c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bc8c0215e748e67baea",
+    "url": "/static/js/main.b9ff3ddd.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0dfd977adb62fbe42d733d5eab8783de.js"
+  "/precache-manifest.f5aca4b6c47e8368e15d0abb115fc1f1.js"
 );
 
 self.addEventListener('message', (event) => {
